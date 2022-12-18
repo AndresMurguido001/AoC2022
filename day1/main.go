1dbbdccc8f0bcefb5b703fbfb1f2de646ff5c2ed
Your Name <dre.murguido@gmail.com>
2022-12-18 14:21:27 -0600

    part 1 completed

--- day1/main.go
+++ day1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -21,29 +22,48 @@ func main()  {
 
   scanner := bufio.NewScanner(f)
 
+  elfs := make([]int, 0)
+  current_total := 0
+
   for scanner.Scan() {
 
     contents := scanner.Text()
-    elfs := make([]int, 0)
-    current_total := 0
 
-    if strings.Compare(contents, "\n") == 0 {
-
+    if strings.Compare(contents, "") == 0 {
+    
       elfs = append(elfs, current_total)
       current_total = 0
       
     } else {
-
+    
       num, _ := strconv.Atoi(contents)
       current_total += num
-
+    
     }
 
   }
-
 
   if err := scanner.Err(); err != nil {
     log.Fatal("Error: Scanner encountered an error")
   }
 
+  maxIndex := findMax(elfs)
+
+  fmt.Println(elfs[maxIndex])
+
+}
+// Return index of maximum value
+func findMax(n []int) int {
+  mi := 0
+  mv := 0
+  for i, x := range n {
+
+    if x > mv {
+      mv = x
+      mi = i
+    }
+
+  }
+
+  return mi
 }
