5b21ec328188368ab54771817bb18d7ac0df64c9
Your Name <dre.murguido@gmail.com>
2022-12-21 10:08:36 -0600

    Day 3 part 1 done

--- day3/main.go
+++ day3/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"unicode"
+)
 
 func main()  {
-  fmt.Printf("TEST: %v", 'c' - 'a' + 1)
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal("Error: Could not open file")
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+  total := 0
+
+	for scanner.Scan() {
+
+		contents := scanner.Text()
+
+    halfway := len(contents)/2
+    part1 := contents[:halfway]
+    part2 := contents[halfway:]
+
+    result := rune(intersection(part1, part2))
+
+    priority := 0
+
+    if (unicode.IsUpper(result)) {
+      priority = int(result) - 'A' + 26 + 1
+    } else {
+      priority = int(result) - 'a' + 1
+    }
+
+    total += int(priority)
+	}
+
+  fmt.Println("TOTAL: ", total)
+
+}
+
+func intersection(part1 string, part2 string) byte {
+  for i := range part1 {
+    for j := range part2 {
+
+      if part1[i] == part2[j] {
+        return part1[i]
+      }
+    }
+  }
+  return 0
 }
